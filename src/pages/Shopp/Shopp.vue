6beb6519a03ef0b44512458964eb1a9c20ddb690
yxx17395887571 <1505856297@qq.com>
2019-12-17 21:09:50 +0800

    yxx change value

--- src/pages/Shopp/Shopp.vue
+++ src/pages/Shopp/Shopp.vue
@@ -1,16 +1,184 @@
 <template>
     <div>
         <top text="购物车"></top>
+        <div class="title">
+            <h2>登录后享受更多优惠<span>去登录 ></span></h2>
+        </div>
+        <div class="content">
+            <h2><b>购物车还是空的</b><span>去逛逛</span></h2>
+        </div>
+        <van-card
+                desc="￥7999"
+                title="小米8 屏幕指纹版"
+                thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+                >
+            <div slot="tags" class="slot">
+                <van-tag plain type="danger">-</van-tag>
+                <van-tag plain type="danger">4</van-tag>
+                <van-tag plain type="danger">+</van-tag>
+              <van-icon name="delete" />
+            </div>
+        </van-card>
+        <div class="guessLike">
+            <img src="../../assets/img/1.png" alt="" class="img1"/>
+            <h2><span>实时推荐你的心心念念</span></h2>
+            <ul>
+                <li v-for="(item,index) in listArr" :key="index">
+                    <img :src="item.img" alt="">
+                    <p>{{item.name}}</p>
+                    <p>{{item.content}}</p>
+                    <p>￥{{item.price}}</p>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
 import Top from '../../components/Top'
 export default {
+    data(){
+        return{
+            listArr:[]
+        }
+    },
+    mounted(){
+        this.$axios.get("https://shiyaming1994.github.io/mi/static/homeGoods.json?page=1").then(res=>{
+            this.listArr=res.data
+        })
+    },
     components:{
         Top
     }
 }
 </script>
 <style lang="less" scoped>
-    
+    .title{
+        width: 100%;
+        height: 1.1rem;
+        background: #fff; 
+        h2{
+            margin-left: 0.3rem;
+            color:#000;
+            font-size: 0.28rem;
+            font-weight: normal;
+            line-height: 1.1rem;
+            span{
+                float: right;
+                margin-right: 0.3rem;
+                font-size: 0.22rem;
+                color: #999;
+            }
+        }   
+    }
+    .content{
+        width: 100%;
+        height: 1.2rem;
+        background: #EBEBEB;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        h2{
+            font-size: 0.26rem;
+            color: #999;
+            text-align: center;
+            font-weight: normal;
+            span{
+                width: 1.24rem;
+                height: 0.4rem;
+                display: inline-block;
+                border: 1px solid #999;
+                margin-left: 0.1rem;
+                color: #000;
+            }
+            b{
+                font-weight: normal;
+                display: inline-block;
+            }
+        }
+    }
+    .guessLike{
+        width: 100%;
+        .img1{
+            width: 2.1rem;
+            height: 0.3rem;
+            display: block;
+            margin: auto;
+            margin-top: 0.5rem;
+        }
+        h2{
+            width: 92%;
+            height: 0.01rem;
+            background: #ccc;
+            margin: auto;
+            margin-top: 0.2rem;
+            margin-bottom: 0.4rem;
+            position: relative;
+            span{
+                width: 2.4rem;
+                height: 0.3rem;
+                background: #fff;
+                position: absolute;
+                text-align: center;
+                font-size: 0.22rem;
+                color: #888;
+                left: 0;
+                top: 0;
+                bottom: 0;
+                right: 0;
+                margin: auto;
+            }
+        }
+        ul{
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            li{
+                width: 49%;
+                box-sizing: border-box;
+                padding-bottom:0.15rem;
+                img{
+                    width: 100%;
+                    height: 3.2rem;
+                }
+                p{
+                    margin-left: 0.2rem;
+                }
+                & p:first-of-type{
+                    font-size: 0.28rem;
+                    margin-top: 0.24rem;
+                    color: #000;
+                }
+                & p:nth-of-type(2){
+                    font-size: 0.2rem;
+                    margin-top: 0.05rem;
+                    color: #999;
+                }
+                & p:last-of-type{
+                    font-size: 0.26rem;
+                    margin-top: 0.05rem;
+                    font-weight: bold;
+                    opacity: 0.8;
+                    color: #EA625B;
+                }
+            }
+        }
+
+    }
+
+    .van-tag{
+        font-size: 18px;
+    }
+    .van-card__desc{
+        margin-top: 0.1rem;
+    }
+    .slot{
+        margin-top: 0.1rem;
+        position: relative;
+    }
+    .van-icon{
+        font-size: 24px;
+        position: absolute;
+        right: 0.2rem;
+    }
 </style>
